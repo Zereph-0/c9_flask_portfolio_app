e36f29bf3007363c55e459ea6e4da36f26240034
Zereph-0 <68672828+Zereph-0@users.noreply.github.com>
2020-07-23 13:05:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = str(name) + "What's up Trig"
+	new_name = str(name) + " What's up Trig"
 	return render_template('index.html', name=new_name)
 
 
